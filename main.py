4f413039df29bd226c4b9cbbdcde8816498a7206
Alvin Aggrey <40688105+AlvinAggrey@users.noreply.github.com>
2024-04-01 14:38:15 +0100

    ver_0.1
    *setup game loop & screen

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-main.py
+import pygame
+from pygame.locals import *
+
+#create screen
+screen = pygame.display.set_mode((288,512))
+pygame.display.set_caption("Flappy Bird")
+
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            running = False
+    
+
+pygame.quit()
